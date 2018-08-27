75234aadf0ef1329983c317657b27ded4ec8d3fa
zhaoming <admin@admin.com>
2018-08-27 10:02:34 +0800

    报错

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -67,35 +67,35 @@
       }
          //this.$router.push({path:'/tongzhidetail/5b7ce580159c52774d73693e'})
          var that = this;
-         if (api) {
-        //设置监听
-        var push = api.require('push');
-        push.setListener(function(ret,err){
-          if(ret){
-            if(ret.data[0]){
-              that.findData(ret.data[0]);
-            }
-            var content = '未知内容';
-            if(that.push_info.desc){
-              content = that.delHtmlTag(that.push_info.desc).substr(0,20)+'...';
-            }
-            api.notification({
-              notify: {
-                title:that.push_info.theme?that.push_info.theme:'未知标题',
-                extra:that.push_info.id?that.push_info.id:'',
-                content:content,
-              },
-            }, function(ret, err) {
-            });
-          }
-        });
-        // 状态栏通知点击事件
-        api.addEventListener({
-          name:'noticeclicked'
-        },function(ret,err){
-          that.$router.push({path:'/tongzhidetail/'+ret.value})
-        });
-      }
+      //    if (api) {
+      //   //设置监听
+      //   var push = api.require('push');
+      //   push.setListener(function(ret,err){
+      //     if(ret){
+      //       if(ret.data[0]){
+      //         that.findData(ret.data[0]);
+      //       }
+      //       var content = '未知内容';
+      //       if(that.push_info.desc){
+      //         content = that.delHtmlTag(that.push_info.desc).substr(0,20)+'...';
+      //       }
+      //       api.notification({
+      //         notify: {
+      //           title:that.push_info.theme?that.push_info.theme:'未知标题',
+      //           extra:that.push_info.id?that.push_info.id:'',
+      //           content:content,
+      //         },
+      //       }, function(ret, err) {
+      //       });
+      //     }
+      //   });
+      //   // 状态栏通知点击事件
+      //   api.addEventListener({
+      //     name:'noticeclicked'
+      //   },function(ret,err){
+      //     that.$router.push({path:'/tongzhidetail/'+ret.value})
+      //   });
+      // }
   
 
   },
